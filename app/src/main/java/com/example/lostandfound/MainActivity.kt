a8d9ee476e4cc908cf26accc574ba6bc8c160858
AnaZ <znaor.a@gmail.com>
2022-02-16 17:05:00 +0100

    import kotlinx, updated the names of textViews and buttons

--- app/src/main/java/com/example/lostandfound/MainActivity.kt
+++ app/src/main/java/com/example/lostandfound/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lostandfound
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/lostandfound/Register.kt
+++ app/src/main/java/com/example/lostandfound/Register.kt
@@ -2,6 +2,7 @@ package com.example.lostandfound
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
