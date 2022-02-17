5e5b4b5029d1639209c7bed2cda98aca6ce8fae8
AnaZ <znaor.a@gmail.com>
2022-02-18 00:53:14 +0100

    new activities

--- app/src/main/java/com/example/lostandfound/MainActivity.kt
+++ app/src/main/java/com/example/lostandfound/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.example.lostandfound
 
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_login.*
+import com.example.lostandfound.entity.ShortNotification
+import com.example.lostandfound.net.RestFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //nameMain =
+
+        newNotifButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewNotification::class.java)
+            startActivity(intent)
+        }
+
+        myNotificationsbutton.setOnClickListener {
+            val intent = Intent(this@MainActivity, MyNotifications::class.java)
+            startActivity(intent)
+        }
+
+        allNotifications.setOnClickListener {
+            val intent = Intent(this@MainActivity, AllNotifications::class.java)
+            startActivity(intent)
+        }
     }
 }
