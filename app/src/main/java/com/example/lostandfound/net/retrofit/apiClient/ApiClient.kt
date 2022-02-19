8aed3924368e9db5c7e750389797308db581b7c9
kocijan <81700527+kocijan@users.noreply.github.com>
2022-02-19 11:52:24 +0100

    adds

--- app/src/main/java/com/example/lostandfound/net/retrofit/apiClient/ApiClient.kt
+++ app/src/main/java/com/example/lostandfound/net/retrofit/apiClient/ApiClient.kt
@@ -1,15 +1,8 @@
 package com.example.lostandfound.net.retrofit.apiClient
 
-<<<<<<< HEAD
-import com.example.lostandfound.net.retrofit.model.NotificationsRequest
-import com.example.lostandfound.net.retrofit.model.NotificationsResponse
-=======
-import com.example.lostandfound.entity.Data
-import com.example.lostandfound.net.retrofit.model.NotificationRequest
-import com.example.lostandfound.net.retrofit.model.NotificationResponse
->>>>>>> 21bc3204e222e83f1405f202120e5d02df3bab99
-import com.example.lostandfound.net.retrofit.model.RegisterRequest
-import com.example.lostandfound.net.retrofit.model.RegisterResponse
+
+import com.example.lostandfound.net.retrofit.model.*
+
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -23,14 +16,12 @@ interface ApiClient {
     @POST("/registerUser")
     fun registerUser(@Body reg: RegisterRequest): Call<RegisterResponse>
 
-<<<<<<< HEAD
     @GET("/pdjetlic2/notifications")
     fun notifications(@Body reg: NotificationsRequest): Call<NotificationsResponse>
-=======
+
     @POST("{username}/putNotification")
     fun pushNotification(@Body notif: NotificationRequest, @Path("username") username: String)
     : Call<NotificationResponse>
->>>>>>> 21bc3204e222e83f1405f202120e5d02df3bab99
 
     companion object {
 //        val BASE_URL = "http://10.7.242.131:8080/"
